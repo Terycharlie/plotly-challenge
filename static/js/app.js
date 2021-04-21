7718607d159d060bfd7c83864d1e0715112bfdc4
terycharlie <terycharlie@yahoo.com>
2021-04-21 18:50:28 -0500

    load data

--- static/js/app.js
+++ static/js/app.js
@@ -3,29 +3,14 @@
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
 
-
+// Filter sample values by selected id
 
 //Use otu_ids as the labels for the bar chart.
 
 //Use otu_labels as the hovertext for the chart.
-// function to create chart
-function plotChart(id) {
-    ////Use the D3 library to read in samples.json
-    d3.json("samples.json").then((data) => {
-        console.log(data)
 
-        // Filter sample values by selected id
-        var filteredSample = data.samples.filter(sample => sample.id === id)[0];
-        console.log(filteredSample);
-        // Get the top 10 OTUs
-        var values = filteredSample.sample_values.slice(0, 10).reverse();
-        var otuids = filteredSample.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
-        var otulabels = filteredSample.otu_labels.slice(0, 10).reverse();
-
-        var trace1 = {
-            x: values,
-            y: otuids,
-            hovertext: otulabels,
-            type: "bar",
-            orientation: "h",
-        };
+var jsonSamples ="samples.json"
+    //Use the D3 library to read in samples.json
+    d3.json("./samples.json").then(function(data) {
+        console.log(data);
+    });
